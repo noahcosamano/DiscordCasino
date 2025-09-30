54ca462f3d3f6d7d23047819edcbbbd0ba743a8c
ChinaRiot <chinariotdev@gmail.com>
2025-09-30 20:21:54 +0100

    WHOOPS wrong imports

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import discord
 from discord.ext import commands
-import baccarat_logic.py
-import baccarat_simulation.py
-import blackjack_logic.py
-import card.py
-import deck.py
-import roulette_logic.py
-import slots_logic.py
-import slots_simulation.py
+import baccarat_logic
+import baccarat_simulation
+import blackjack_logic
+import card
+import deck
+import roulette_logic
+import slots_logic
+import slots_simulation
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix = "r.", self_bot = False, intents=intents)
